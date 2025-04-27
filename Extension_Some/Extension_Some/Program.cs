258e8154d55ab7ef0995201b7f7c41c6438f4e5a
Karen-xgit <ananyan2020>
2025-04-28 02:39:05 +0400

    add project Extension_Some

---
+++ Extension_Some/Extension_Some/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Extension_Some;
+
+static class Extension
+{
+    public static bool Some<T>(this List<T> list, Func<T, bool> func)
+    {
+       foreach(var item in list)
+       {
+        if(func(item))
+        {
+            return true;
+        }
+       }
+       return false;
+    }
+}
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> list = new List<int>(){ 1, 2, 3, 4, 5, -6, 7, 8, 9};
+
+        
+        bool result = list.Some(a => a < 0);
+        System.Console.WriteLine(result);
+    }
+}
