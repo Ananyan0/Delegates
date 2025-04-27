017162ac6d20aa414aeb031ae6a8608235792d98
Karen-xgit <ananyan2020>
2025-04-28 02:34:04 +0400

    add project Memoize

---
+++ Memoize/Memoize/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Memoize;
+
+class Program
+{
+
+    public static Func<T, T> Memoize<T>(Func <T, T> func)
+    {
+
+        Dictionary<T, T> cache = new Dictionary<T, T>();
+       
+        return (x) => {
+            if (cache.ContainsKey(x)) 
+            {
+                return cache[x];
+            }
+            else
+            {
+                T result = func(x);
+                cache.Add(x, result);
+                return result;
+            }
+        };
+    }
+
+
+    static void Main(string[] args)
+    {
+        Func<int, int> factorial = (int p) => {
+            if(p == 0) return 1;
+            int temp = 1;
+            for(int i = 2; i <= p; i++)
+            {
+                temp *= i;
+            }
+            return temp;
+        };
+
+        var memoFact = Memoize(factorial);
+
+        
+        Console.WriteLine((memoFact(5))); // 120
+        Console.WriteLine((memoFact(4)));
+        Console.WriteLine((memoFact(5)));
+        Console.WriteLine((memoFact(5)));
+        Console.WriteLine((memoFact(8)));
+        
+    }
+}
